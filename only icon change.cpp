893924853a0d0bdea355119e74baa47934a93f47
CStone6 <tobycat133@gmail.com>
2025-04-27 11:51:51 -0400

    Create only icon change.cpp

---
+++ only icon change.cpp
@@ -0,0 +1,10 @@
+const Theme g_themeWinXP = {{
+    ThemeTargetStyles{L"Taskbar.ExperienceToggleButton#LaunchListButton[AutomationProperties.AutomationId=StartButton] > Taskbar.TaskListButtonPanel > Border#BackgroundElement", {
+        L"Background:=<ImageBrush Stretch=\"None\" ImageSource=\"B:\\pictures\\starticon2.png\" />"}},
+    ThemeTargetStyles{L"Taskbar.ExperienceToggleButton#LaunchListButton[AutomationProperties.AutomationId=StartButton] > Taskbar.TaskListButtonPanel > Microsoft.UI.Xaml.Controls.AnimatedVisualPlayer#Icon", {
+        L"Visibility=Collapsed"}},
+    ThemeTargetStyles{L"Taskbar.ExperienceToggleButton#LaunchListButton[AutomationProperties.AutomationId=StartButton]", {
+        L"MaxWidth=48",
+        L"Margin=0,0,0,0",
+        L"CornerRadius=0"}},
+}};
